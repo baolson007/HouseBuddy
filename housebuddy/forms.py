6bc46794be4722a36d7efdb4fe186188093400a5
Ben Olson <benolson3154@gmail.com>
2021-07-09 13:04:00 -0700

    registration form with validation

--- housebuddy/forms.py
+++ housebuddy/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from housebuddy.models import User
+
 
 class RegisterForm(FlaskForm):
-	username = StringField(label='User Name:')
-	email = StringField(label='Email Address:')
-	password = PasswordField(label='Password:')
-	password_confirm = PasswordField(label='Confirm Password:')
+
+	def validate_username(self, username_to_validate):
+		user = User.query.filter_by(username=username_to_validate.data).first()
+		if user:
+			raise ValidationError(f'Username \"{username_to_validate.data}\" already exists, please a different username')
+
+	def validate_email(self, email_to_validate):
+		email = User.query.filter_by(email=email_to_validate.data).first()
+		if email:
+			raise ValidationError(f'Email address \"{email_to_validate.data}\" already exists, please try different email address')
+
+	username = StringField(label='User Name:', validators=[Length(min=2, max=40), DataRequired()])
+	email = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+	password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+	password_confirm = PasswordField(label='Confirm Password:', validators=[EqualTo('password'), DataRequired()])
 	submit = SubmitField(label='Register Account')
--- housebuddy/routes.py
+++ housebuddy/routes.py
@@ -1,7 +1,8 @@
 from housebuddy import app
-from flask import render_template
-from housebuddy.models import MaintenanceItem
+from flask import render_template, redirect, url_for, flash
+from housebuddy.models import MaintenanceItem, User
 from housebuddy.forms import RegisterForm
+from housebuddy import db
 
 @app.route('/')
 @app.route('/home')
@@ -29,7 +30,18 @@ def maintenance():
     items = MaintenanceItem.query.all()
     return render_template('maintenance.html', items=items, username = 'TestUser');
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data,
+                                email=form.email.data,
+                                password_hash=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('home_page'))
+    if form.errors != {}:
+        for msg in form.errors.values():
+            flash(f'Error in registration: {msg}', category='danger')
     return render_template('register.html', form=form)
